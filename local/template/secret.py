6988375021e848f35fc5dae5d8ea0ba828d5b4a3
dbrannonhoward <dbrannonhoward@gmail.com>
2021-05-02 13:30:51 -0400

    fix: update data structure

--- local/template/secret.py
+++ local/template/secret.py
@@ -1,9 +1,13 @@
-machine_receive_01 = {
-    'ip address':   '',
-    'password':     '',
-    'user_name':    ''
-}
-
 machine_dispatch_01 = {
-    'path to monitor':  '',
+    # for example : /home/dispatch/media/
+    'path to media':  '',
+}
+machine_receive_01 = {
+    # for example : abc.def.g.h
+    'ip address':       '',
+    # not used
+    'password':         '',
+    'user_name':        '',
+    # for example : Videos/ would send the files to /home/username/Videos/ on the remote
+    'path to receive':  ''
 }
