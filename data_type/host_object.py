a2474271e4ff36f0f46da643ff4ff3274d432ff4
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-02 13:18:45 -0400

    add: build src/dest

--- data_type/host_object.py
+++ data_type/host_object.py
@@ -1,29 +1,52 @@
-from local.secret import machine_dispatch_01
-from local.secret import machine_receive_01
+from os import walk
 from pathlib import Path
 
 
 class LocalDispatcher:
     def __init__(self,
-                 path_to_media:     Path,
+                 machine: dict,
                  clean_up=False,
                  wait_seconds=60):
 
         """
-        :param path_to_media: Path, the directory-to-monitor for media-to-send
+        :param machine: relevant details for the dispatching machine
         :param clean_up: bool, whether or not to delete sent files
         :param wait_seconds: int, seconds to wait between dispatcher checks
         """
-        self.path_to_media = path_to_media
+        self.path_to_media = machine['path to media']
         self.clean_up = clean_up
         self.wait_seconds = wait_seconds
+        self.source = self.build_source()
+
+    def build_source(self) -> str:
+        source = ''
+        source = source + self.path_to_media
+        return source
+
+    def media_path_has_content(self) -> bool:
+        found_files = list()
+        for root, _, files in walk(self.path_to_media):
+            for file in files:
+                found_files.append(Path(root, file))
+        return True if found_files else False
 
 
 class LocalReceiver:
     def __init__(self,
-                 ip_address:    str,
-                 password:      str,
-                 user_name:     str):
-        self.ip_address = ip_address
-        self.password = password
-        self.user_name = user_name
+                 machine: dict):
+        # save
+        self.ip_address = machine['ip address']
+        self.password = machine['password']
+        self.user_name = machine['user_name']
+        self.path_to_receive = machine['path to receive']
+        # build
+        self.destination = self.build_destination()
+
+    def build_destination(self) -> str:
+        destination = ''
+        destination = destination + self.user_name
+        destination = destination + '@'
+        destination = destination + self.ip_address
+        destination = destination + ':'
+        destination = destination + self.path_to_receive
+        return destination
