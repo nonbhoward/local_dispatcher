31f70ff19023314104bbb45fb47afb36b341c7ba
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-02 13:18:16 -0400

    add: simple loop

--- data_type/host_object.py
+++ data_type/host_object.py
@@ -5,14 +5,18 @@ from pathlib import Path
 
 class LocalDispatcher:
     def __init__(self,
-                 clean_up:          bool,
-                 path_to_media:     Path):
+                 path_to_media:     Path,
+                 clean_up=False,
+                 wait_seconds=60):
+
         """
         :param path_to_media: Path, the directory-to-monitor for media-to-send
         :param clean_up: bool, whether or not to delete sent files
+        :param wait_seconds: int, seconds to wait between dispatcher checks
         """
         self.path_to_media = path_to_media
         self.clean_up = clean_up
+        self.wait_seconds = wait_seconds
 
 
 class LocalReceiver:
--- script/run.py
+++ script/run.py
@@ -1,3 +1,17 @@
-from data_type.host_objects import LocalDispatcher
-from data_type.host_objects import LocalReceiver
+from data_type.command_object import Rsync
+from data_type.host_object import LocalDispatcher
+from data_type.host_object import LocalReceiver
+from local.secret import machine_dispatch_01
+from local.secret import machine_receive_01
+from time import sleep
+ld = LocalDispatcher(machine=machine_dispatch_01)
+del machine_dispatch_01
+lr_01 = LocalReceiver(machine=machine_receive_01)
+del machine_receive_01
+rs = Rsync(source=ld.source, destination=lr_01.destination)
 
+while True:
+    # if folder has content, execute command
+    if ld.media_path_has_content():
+        rs.sync()
+    sleep(ld.wait_seconds)
